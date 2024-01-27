57ea2001436e590f1dfcacdc6d94dc1c17fa8e15
Luiz Antonio <luiz8907@hotmail.com>
2024-01-27 17:43:43 -0300

    FRONT_31 - Ajuste de variaveis de ambiente

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,7 @@
 export const environment = {
-    // production: false,
-    // //apiUrl : 'http://localhost:8000/api/',
-    // apiUrl : 'http://192.168.0.106:8000/api/',
-    // //imagePath : 'http://localhost:8000',
-    // imagePath : 'http://192.168.0.106:8000',
-    production: true,
-    apiUrl : 'https://api.luizantonio.dev.br/api/',
-    imagePath : 'https://api.luizantonio.dev.br',
-};
+    production: false,
+    //apiUrl : 'http://192.168.0.106:8000/api/',
+    //imagePath : 'http://192.168.0.106:8000',
+    apiUrl : 'http://localhost:8000/api/',
+    imagePath : 'http://localhost:8000',
+}
